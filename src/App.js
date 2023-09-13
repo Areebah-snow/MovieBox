85209d532c72828e7ffd33a9f7c50d37ff02c8a1
Areebah-snow <mudathiraisha657@gmail.com>
2023-09-13 18:13:46 +0100

    api issue

--- src/App.js
+++ src/App.js
@@ -10,25 +10,27 @@ import MovieList from './components/MovieList';
 function App() {
 
   const API_KEY = process.env.REACT_API_KEY
-  const API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
-  
-  // State to store the fetched movies
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
+   
+    const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+
     // Fetch movie data from the API
     fetch(API_URL)
-      .then((res) => res.json()) // Parse the response as JSON
+      .then((res) => res.json()) 
       .then((data) => {
         // Update the movies state with the fetched data
         setMovies(data.results);
+
         // Log the fetched movie data
         console.log('Fetched movie data:', data.results);
       })
       .catch((error) => {
+   
         console.error('Error fetching movies:', error);
-      });
-  }, []); 
+      });
+  }, []);
 
 
    
